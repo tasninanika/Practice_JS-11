25a0a777d030694e3854760f27792fb8fdfb8740
anikatasnin <jarintasnin09@gmail.com>
2024-08-28 10:15:42 +0000

    updated

--- practice-2.js
+++ practice-2.js
@@ -1,10 +1,10 @@
 const friends = ['Jarin', 'Tasnin', 'Anika', 'Mina', 'Rina'];
 let evenFriends = [];
-const arr = (friendsName) =>{
 
-   for(let i = 0; i < friends.length; i++){
-    if(friends[i].length % 2 === 0){
-     evenFriends.push(friends[i]);
+const arr = (friendsName) =>{
+   for(const friend of friends){
+    if(friend.length % 2 === 0){
+     evenFriends.push(friend);
     }   
    } 
    return evenFriends;
