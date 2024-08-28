0359ee84a56a60ed204e5d4dd0761beb2643f8ab
anikatasnin <jarintasnin09@gmail.com>
2024-08-28 10:12:50 +0000

    updated

--- practice-3.js
+++ practice-3.js
@@ -1,10 +1,10 @@
-const arr = [2, 3, 4, 5, 6];
+ const arr = [2, 3, 4, 5, 6];
 
 const avgFun = (numbers) =>{
     let sum = 0;
-    for(let i = 0; i < arr.length; i++){
-        const squareArr = Math.pow(arr[i], 2);
-        sum += squareArr;
+    for(const num of arr){
+        const sqr = Math.pow(num, 2);
+        sum += sqr;
     }
     const avg = sum / arr.length;
     return avg;
